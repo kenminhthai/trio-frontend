71c917fa2095e895f29c78d43104a49b09829158
Luke Rollans <hello@lukerollans.me>
2017-06-19 20:44:14 +0800

    Setup SASS preprocesing (without ejecting!)

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import Trio from './Trio';
 import registerServiceWorker from './registerServiceWorker';
 
+import './App.css'
+
 ReactDOM.render(<Trio />, document.getElementById('root'));
 registerServiceWorker();
