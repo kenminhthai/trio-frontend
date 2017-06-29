7878b197eeb65d85e37672943d40647a26d09904
Luke Rollans <hello@lukerollans.me>
2017-06-29 21:12:41 +0800

    Wire up the selected language reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import languagesReducer from './languages'
+import selectedLanguageReducer from './selected-language'
 
 export default combineReducers({
-  languages: languagesReducer
+  languages: languagesReducer,
+  selectedLanguage: selectedLanguageReducer
 })
