b9cb6b7795ba6a08d1756723c4ab62acbaa8a7b0
Luke Rollans <hello@lukerollans.me>
2017-07-01 21:28:09 +0800

    Refactor styling of induction

--- src/components/induction.js
+++ src/components/induction.js
@@ -1,26 +1,33 @@
 import React from 'react'
 import trioLogo from '../assets/images/brand/trio-logo.svg'
+import doctor from '../assets/images/characters/doctor.svg'
 
 const Induction = () => {
   return (
     <div className="induction">
-      <article>
-        <h1>
-          <img src={trioLogo} alt="Trio" width="167" />
-        </h1>
+      <div className="wrap">
+        <article>
+          <h1>
+            <img src={trioLogo} alt="Trio" width="167" />
+          </h1>
 
-        <h2>
-          The easiest way to discover and share Duolingo Clubs
-        </h2>
+          <h2>
+            The easiest way to discover and share Duolingo Clubs
+          </h2>
 
-        <h3>
-          We can only support English speakers right now, sorry!
-        </h3>
+          <h3>
+            We can only support English speakers right now, sorry!
+          </h3>
 
-        <button className="btn btn-success btn-dropdown choose-language">
-          Choose a Language
-        </button>
-      </article>
+          <button className="btn btn-success btn-dropdown choose-language">
+            Choose a Language
+          </button>
+        </article>
+
+        <div className="doctor">
+          <img className="doctor" width="330" src={doctor} alt="" />
+        </div>
+      </div>
     </div>
   )
 }
