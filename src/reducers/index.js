6a4a6777975b40c6c8c958aa481f97f84bfbb27c
Luke Rollans <hello@lukerollans.me>
2017-07-01 15:11:22 +0800

    Add clubs reducer to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import languagesReducer from './languages'
+import clubsReducer from './clubs'
 import selectedLanguageReducer from './selected-language'
 
 export default combineReducers({
   languages: languagesReducer,
+  clubs: clubsReducer,
   selectedLanguage: selectedLanguageReducer
 })
