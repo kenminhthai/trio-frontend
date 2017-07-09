c4d9676d12e0a70e12d45ffdbddb2ed42aa7db14
Luke Rollans <hello@lukerollans.me>
2017-07-09 11:13:49 +0800

    Add plain form fields

--- src/components/__tests__/club-form.js
+++ src/components/__tests__/club-form.js
@@ -12,4 +12,35 @@ describe('ClubForm', () => {
     const wrapper = shallow(<ClubForm />)
     expect(wrapper.hasClass('club-form')).toBe(true)
   })
+
+  it('renders a required field for Club code', () => {
+    const wrapper = shallow(<ClubForm />)
+    expect(wrapper.containsMatchingElement(
+      <input
+        type="text"
+        name="club[code]"
+        placeholder="XYDZ"
+        required />
+    )).toBe(true)
+  })
+
+  it('renders an optional field for Club name', () => {
+    const wrapper = shallow(<ClubForm />)
+    expect(wrapper.containsMatchingElement(
+      <input
+        type="text"
+        name="club[name]"
+        placeholder="Perth Deutsch" />
+    )).toBe(true)
+  })
+
+  it('renders an optional field for Club description', () => {
+    const wrapper = shallow(<ClubForm />)
+    expect(wrapper.containsMatchingElement(
+      <textarea
+        name="club[description]"
+        placeholder="We live in Perth and we love to practise German!">
+      </textarea>
+    )).toBe(true)
+  })
 })
--- src/components/club-form.js
+++ src/components/club-form.js
@@ -3,7 +3,23 @@ import React from 'react'
 const ClubForm = (props) => {
   return (
     <div className="club-form">
-      <p>I am the Club form!</p>
+      <form>
+        <div className="form-field">
+          <label>Club code</label>
+          <input type="text" name="club[code]" placeholder="XYDZ" required/>
+        </div>
+
+        <div className="form-field">
+          <label>Name (optional)</label>
+          <input type="text" name="club[name]" placeholder="Perth Deutsch" />
+        </div>
+
+        <div className="form-field">
+          <label>Short description (optional)</label>
+          <textarea name="club[description]" placeholder="We live in Perth and we love to practise German!">
+          </textarea>
+        </div>
+      </form>
     </div>
   )
 }
